402b1a55d5939c37b71261698659d98e00e0e917
Matthew Bucy <mbucy347@gmail.com>
2023-12-21 21:54:49 -0500

    we couldn't figure ts out

---
+++ 03/matt.py
@@ -0,0 +1,11 @@
+def solution1(filename):
+    pass   
+
+def solution2(filename):
+    pass
+
+
+file = "sample_input.txt" #input("what file is your input?\n")
+
+# print(f"Solution 1: {solution1(file)}")
+print(f"Solution 2: {solution2(file)}")
