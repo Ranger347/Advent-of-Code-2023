ef6773923a9a324b16be1142a6bd5682c61393e5
Patrick Hultberg <90772733+Cicerotheprogrammer@users.noreply.github.com>
2023-12-21 19:47:26 -0500

    me

--- 01/solution.py
+++ 01/solution.py
@@ -1,25 +1,22 @@
 
-def solution1():
-    sum = 0
-    with open("./input.txt") as input:
+valid_digits = {
+    "one" : "1",
+    "two" : "2",
+    "three" : "3",
+    "four" : "4",
+    "five" : "5",
+    "six" : "6",
+    "seven" : "7",
+    "eight" : "8",
+    "nine" : "9"
+}
 
-<<<<<<< HEAD
 sum = 0
 with open("./input1.txt") as input:
-=======
-        # loop through each line
-        for line in input:
->>>>>>> e7084da2afde25f3a644a5cbb354dd47bd969d3d
 
-            num = ""
-            # in each line, go forwards and backwards through the line to find numbers
-            for forward in line:
-                if (forward.isdigit()):
-                    # print(forward)
-                    num = forward
-                    break
+    # loop through each line
+    for line in input:
 
-<<<<<<< HEAD
         for key, value in valid_digits.items():
             if (key in line):
                 line = line.replace(key, f"{key}{value}{key}")
@@ -43,64 +40,3 @@ with open("./input1.txt") as input:
         sum += int(num)
         
 print(sum)
-=======
-            for backward in line[::-1]:
-                if (backward.isdigit()):
-                    # print(backward)
-                    num += backward
-                    break
-            
-            # print(num)
-
-            sum += int(num)
-                
-    return sum
-    
-
-def solution2(): 
-
-    valid_digits = {
-        "one" : "1",
-        "two" : "2",
-        "three" : "3",
-        "four" : "4",
-        "five" : "5",
-        "six" : "6",
-        "seven" : "7",
-        "eight" : "8",
-        "nine" : "9"
-    }
-
-    sum = 0
-    with open("./input.txt") as input:
-
-        # loop through each line
-        for line in input:
-
-            for key, value in valid_digits.items():
-                if (key in line):
-                    line = line.replace(key, f"{key}{value}{key}")
-
-            num = ""
-            # in each line, go forwards and backwards through the line to find numbers
-            for forward in line:
-                if (forward.isdigit()):
-                    # print(forward)
-                    num = forward
-                    break
-
-            for backward in line[::-1]:
-                if (backward.isdigit()):
-                    # print(backward)
-                    num += backward
-                    break
-            
-            # print(num)
-
-            sum += int(num)
-                
-    return sum
-
-print(f"Solution 1: {solution1()}")
-print(f"Solution 2: {solution2()}")
->>>>>>> e7084da2afde25f3a644a5cbb354dd47bd969d3d
